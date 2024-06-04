0200f1ccbbf3890cfd14be21aa0e1a9bdb97be0d
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-06-04 01:49:11 +0100

    update

---
+++ View/CalendarTab.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DentistStudioApp.View
+{
+    /// <summary>
+    /// Interaction logic for CalendarTab.xaml
+    /// </summary>
+    public partial class CalendarTab : Window
+    {
+        public CalendarTab()
+        {
+            InitializeComponent();
+        }
+
+        private void CalendarForm_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Hello!");
+        }
+    }
+}
