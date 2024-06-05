8a02ea6d9ee595762f71b8b7c6f16c68d663a50b
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-06-05 11:11:53 +0100

    update

---
+++ Model/Invoice/Invoice.cs
@@ -0,0 +1,46 @@
+﻿using Backend.ExtensionMethods;
+using Backend.Model;
+using FrontEnd.Model;
+using System.Data.Common;
+
+namespace DentistStudioApp.Model
+{
+    [Table(nameof(Invoice))]
+    public class Invoice : AbstractModel
+    {
+        #region backing fields
+        private long _invoiceid;
+        private DateTime? _doi;
+        private double _discount;
+        private PaymentType? _paymentType;
+        private bool _paid;
+        #endregion
+
+        #region properties
+        [PK]
+        public long InvoiceID { get => _invoiceid; set => UpdateProperty(ref value, ref _invoiceid); }
+        [Field]
+        public DateTime? DOI { get => _doi; set => UpdateProperty(ref value, ref _doi); }
+        [Field]
+        public double Discount { get => _discount; set => UpdateProperty(ref value, ref _discount); }
+        [FK]
+        public PaymentType? PaymentType { get => _paymentType; set => UpdateProperty(ref value, ref _paymentType); }
+        #endregion
+
+        public Invoice() 
+        { 
+            
+        }
+
+        public Invoice(DbDataReader reader)
+        {
+            _invoiceid = reader.GetInt64(0);
+            _doi = reader.TryFetchDate(1);
+            _discount = reader.GetDouble(2);
+            _paymentType = new(reader.GetInt64(3));
+            _paid = reader.GetBoolean(4);
+        }
+
+        public override ISQLModel Read(DbDataReader reader) => new Invoice(reader);
+    }
+}
---
+++ Model/Invoice/PaymentType.cs
@@ -0,0 +1,34 @@
+﻿using Backend.Model;
+using FrontEnd.Model;
+using System.Data.Common;
+
+namespace DentistStudioApp.Model
+{
+    public class PaymentType : AbstractModel
+    {
+        #region backing fields
+        long _paymentTypeID;
+        string _paymentBy = string.Empty;
+        #endregion
+
+        #region properties
+        public long PaymentTypeID { get => _paymentTypeID; set => UpdateProperty(ref value, ref _paymentTypeID); }
+        public string PaymentBy { get => _paymentBy; set => UpdateProperty(ref value, ref _paymentBy); }
+        #endregion
+
+        public PaymentType() { }
+
+        public PaymentType(long id) => _paymentTypeID = id;
+
+        public PaymentType(DbDataReader reader) 
+        {
+            _paymentTypeID = reader.GetInt64(0);
+            _paymentBy = reader.GetString(1);
+        }
+
+        public override ISQLModel Read(DbDataReader reader) => new PaymentType(reader);
+
+        public override string? ToString() => $"{PaymentBy}";
+
+    }
+}
