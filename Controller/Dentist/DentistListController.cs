733442aa2f9baf6c10fd6871f646228dfc102d5f
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-06-05 10:46:06 +0100

    update

--- Controller/Dentist/DentistListController.cs
+++ Controller/Dentist/DentistListController.cs
@@ -10,27 +10,41 @@ namespace DentistStudioApp.Controller
     public class DentistListController : AbstractFormListController<Dentist>
     {
         public SourceOption ClinicOptions { get; private set; }
-        public override string SearchQry { get; set; } = string.Empty;
+        public override string SearchQry { get; set; } = $"SELECT * FROM {nameof(Dentist)} WHERE (LOWER(FirstName) LIKE @name OR LOWER(LastName) LIKE @name)";
         public RecordSource Clinics { get; private set; } = new(DatabaseManager.Find<Clinic>()!);
         public override int DatabaseIndex => 10;
 
         public DentistListController() 
         {
             ClinicOptions = new(Clinics, "ClinicName");
+            OpenWindowOnNew = false;
+            AfterUpdate += OnAfterUpdate;
+        }
+
+        private async void OnAfterUpdate(object? sender, AfterUpdateArgs e)
+        {
+            if (!e.Is(nameof(Search))) return;
+            var results = await Task.Run(SearchRecordAsync);
+            AsRecordSource().ReplaceRange(results);
+
+            if (sender is not FilterEventArgs filterEvtArgs)
+                GoFirst();
         }
 
         public override void OnOptionFilter(FilterEventArgs e)
         {
+            QueryBuiler.Clear();
+            QueryBuiler.AddCondition(ClinicOptions.Conditions(QueryBuiler));
+            OnAfterUpdate(e, new(null, null, nameof(Search)));
         }
 
-        public override Task<IEnumerable<Dentist>> SearchRecordAsync()
+        public override async Task<IEnumerable<Dentist>> SearchRecordAsync()
         {
-            throw new NotImplementedException();
+            QueryBuiler.AddParameter("name", Search.ToLower() + "%");
+            QueryBuiler.AddParameter("name", Search.ToLower() + "%");
+            return await CreateFromAsyncList(QueryBuiler.Query, QueryBuiler.Params);
         }
 
-        protected override void Open(Dentist? model)
-        {
-            throw new NotImplementedException();
-        }
+        protected override void Open(Dentist? model) { }
     }
 }
--- Controller/Treatments/ServiceListController.cs
+++ Controller/Treatments/ServiceListController.cs
@@ -6,22 +6,37 @@ namespace DentistStudioApp.Controller
 {
     public class ServiceListController : AbstractFormListController<Service>
     {
-        public override string SearchQry { get; set; } = string.Empty;
+        public override string SearchQry { get; set; } = $"SELECT * FROM {nameof(Service)} WHERE LOWER(ServiceName) LIKE @name";
 
         public override int DatabaseIndex => 8;
 
-        public override void OnOptionFilter(FilterEventArgs e)
+        public ServiceListController() 
         {
+            AfterUpdate += OnAfterUpdate;
+            OpenWindowOnNew = false;
         }
 
-        public override Task<IEnumerable<Service>> SearchRecordAsync()
+        private async void OnAfterUpdate(object? sender, AfterUpdateArgs e)
         {
-            throw new NotImplementedException();
+            if (e.Is(nameof(Search))) 
+            {
+                var results = await Task.Run(SearchRecordAsync);
+                AsRecordSource().ReplaceRange(results);
+                GoFirst();
+            }
+        }
+
+        public override void OnOptionFilter(FilterEventArgs e) { }
+
+        public override async Task<IEnumerable<Service>> SearchRecordAsync()
+        {
+            QueryBuiler.AddParameter("name", Search.ToLower() + "%");
+            return await CreateFromAsyncList(QueryBuiler.Query, QueryBuiler.Params);
         }
 
         protected override void Open(Service? model)
         {
-            throw new NotImplementedException();
+
         }
     }
 }
