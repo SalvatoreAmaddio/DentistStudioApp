a2f292e3911d1acc9f3dcfce209eac469ea2ee13
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-06-14 13:02:53 +0100

    update

--- Controller/Survey/SurveyController.cs
+++ Controller/Survey/SurveyController.cs
@@ -1,10 +1,21 @@
 ﻿using DentistStudioApp.Model;
 using FrontEnd.Controller;
+using FrontEnd.Utils;
 
 namespace DentistStudioApp.Controller
 {
     public class SurveyController : AbstractFormController<Survey>
     {
         public override int DatabaseIndex => 3;
+
+        protected override bool Delete(Survey? model)
+        {
+            bool result = base.Delete(model);
+            if (result)
+            {
+                Helper.GetActiveWindow()?.Close();
+            }
+            return result;
+        }
     }
 }
