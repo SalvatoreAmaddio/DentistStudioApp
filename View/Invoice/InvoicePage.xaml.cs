a2f2b3a9355e84b0f8f4e3c86e77cfed8c275bed
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-06-26 23:59:59 +0100

    update

---
+++ View/Invoice/InvoicePage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using DentistStudioApp.Model;
+using FrontEnd.Reports;
+
+namespace DentistStudioApp.View
+{
+    /// <summary>
+    /// Interaction logic for Invoice.xaml
+    /// </summary>
+    public partial class InvoicePage : ReportPage, IClonablePage
+    {
+        private readonly Invoice _invoice;
+        public InvoicePage(Invoice invoice)
+        {
+            InitializeComponent();
+            _invoice = invoice;
+
+            InvoiceID.Content = $"Invoice# {_invoice.InvoiceID}";
+            DOI.Content = $"{_invoice.DOI}";
+
+            ToBePaid.Content = (_invoice.Paid) ? "Paid by" : "To be paid by";
+            PaymentMethod.Content = _invoice.PaymentType?.PaymentBy;
+           
+            Total.Content = _invoice.Amount;
+            Deposit.Content = _invoice.Deposit;
+            TotalDue.Content = _invoice.TotalDue;
+            Discount.Content = _invoice.Discount;
+        }
+
+        public ReportPage CloneMe() => new InvoicePage(_invoice);
+    }
+}
